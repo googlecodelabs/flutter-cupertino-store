6bd7ee9bbfa7ccfa710e68578f02491ae080cf23
Brett Morgan <brettmorgan@google.com>
2019-03-14 15:11:30 +1100

    Splitting out codelab steps

---
+++ step-02/lib/styles.dart
@@ -0,0 +1,31 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
+
+abstract class Styles {
+  static const TextStyle headlineText = TextStyle(
+    color: Color.fromRGBO(0, 0, 0, 0.8),
+    fontSize: 32,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle productRowItemName = TextStyle(
+    color: Color.fromRGBO(0, 0, 0, 0.8),
+    fontSize: 18,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const TextStyle productRowItemPrice = TextStyle(
+    color: Color(0xFF8E8E93),
+    fontSize: 13,
+    fontWeight: FontWeight.w300,
+  );
+
+  static const Color appBackground = Color(0xffd0d0d0);
+
+}
