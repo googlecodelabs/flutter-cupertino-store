773c58ec110b9ba7830d65e7a120fef57769e37c
Brett Morgan <brettmorgan@google.com>
2019-03-27 17:12:06 +1100

    Fixes for @xster's review

--- step-05/lib/main.dart
+++ step-05/lib/main.dart
@@ -20,6 +20,8 @@ import 'app.dart';
 import 'model/app_state_model.dart';
 
 void main() {
+  // This app is designed only to work vertically, so we limit
+  // orientations to portrait up and down.
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 
