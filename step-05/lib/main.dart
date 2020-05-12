a1b2c265eea17687d95db1ebe9652085256fcfe8
Brett Morgan <brett.morgan@gmail.com>
2020-05-12 14:23:50 +1000

    step-05

--- step-05/lib/main.dart
+++ step-05/lib/main.dart
@@ -13,21 +13,15 @@
 // limitations under the License.
 
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 import 'app.dart';
 import 'model/app_state_model.dart';
 
 void main() {
-  // This app is designed only to work vertically, so we limit
-  // orientations to portrait up and down.
-  SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-
   return runApp(
     ChangeNotifierProvider<AppStateModel>(
-      builder: (context) => AppStateModel()..loadProducts(),
+      create: (context) => AppStateModel()..loadProducts(),
       child: CupertinoStoreApp(),
     ),
   );
