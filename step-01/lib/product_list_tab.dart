12e5157d145d1e035f8716973c673ff44739c4f8
Brett Morgan <brett.morgan@gmail.com>
2020-05-12 15:10:30 +1000

    fixup product list tab

--- step-01/lib/product_list_tab.dart
+++ step-01/lib/product_list_tab.dart
@@ -16,39 +16,17 @@ import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 
 import 'model/app_state_model.dart';
-import 'product_row_item.dart';
 
 class ProductListTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<AppStateModel>(
       builder: (context, model, child) {
-        final products = model.getProducts();
-        return CustomScrollView(
-          semanticChildCount: products.length,
+        return const CustomScrollView(
           slivers: <Widget>[
-            const CupertinoSliverNavigationBar(
+            CupertinoSliverNavigationBar(
               largeTitle: Text('Cupertino Store'),
             ),
-            SliverSafeArea(
-              top: false,
-              minimum: const EdgeInsets.only(top: 8),
-              sliver: SliverList(
-                delegate: SliverChildBuilderDelegate(
-                  (context, index) {
-                    if (index < products.length) {
-                      return ProductRowItem(
-                        index: index,
-                        product: products[index],
-                        lastItem: index == products.length - 1,
-                      );
-                    }
-
-                    return null;
-                  },
-                ),
-              ),
-            )
           ],
         );
       },
