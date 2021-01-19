62a766e0b3fea243149db52c839289b2597c5a9c
Abhishek01039 <abhishekghaskata1999@gmail.com>
2021-01-19 11:11:15 +0530

    converted stateful widget to stateless widget

--- step-01/lib/search_tab.dart
+++ step-01/lib/search_tab.dart
@@ -14,14 +14,7 @@
 
 import 'package:flutter/cupertino.dart';
 
-class SearchTab extends StatefulWidget {
-  @override
-  _SearchTabState createState() {
-    return _SearchTabState();
-  }
-}
-
-class _SearchTabState extends State<SearchTab> {
+class SearchTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const CustomScrollView(
